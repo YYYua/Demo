b5bb95336c1335dbcb085f913247ef2f614aa27f
lishuangyuan <10767818502qq.com>
2020-03-20 21:25:41 +0800

    helloworld

---
+++ helloWorld.java
@@ -0,0 +1,11 @@
+public class Helloword {
+	
+	public  void  static main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("2");
+		System.out.println("Hello World");
+		System.out.println("1");
+		System.out.println("Hello World");
+		
+	}
+}
